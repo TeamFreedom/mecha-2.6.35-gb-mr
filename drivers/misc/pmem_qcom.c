9e6bd8948f99865a3b331096834e02d515da48c3
xoomdev <xoomdevnet@gmail.com>
2011-10-30 12:16:48 -0500

    pmem: Add PMEM_CACHE_FLUSH ioctl

--- drivers/misc/pmem_qcom.c
+++ drivers/misc/pmem_qcom.c
@@ -2305,6 +2305,11 @@ static long pmem_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 
 	case PMEM_CACHE_FLUSH:
 	{
+        struct pmem_region region;
+        if (copy_from_user(&region, (void __user *)arg,
+                sizeof(struct pmem_region)))
+            return -EFAULT;
+        flush_pmem_file(file, region.offset, region.len);
 		break;
 	}
 	case PMEM_CLEAN_INV_CACHES:
