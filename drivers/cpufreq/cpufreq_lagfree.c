b0ab08fb99297b6056489edb9f03313420015353
xoomdev <xoomdevnet@gmail.com>
2011-11-06 09:19:25 -0600

    Adjust lagfree a bit

--- drivers/cpufreq/cpufreq_lagfree.c
+++ drivers/cpufreq/cpufreq_lagfree.c
@@ -35,11 +35,11 @@
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_FREQUENCY_UP_THRESHOLD			(50)
-#define DEF_FREQUENCY_DOWN_THRESHOLD		(15)
+#define DEF_FREQUENCY_UP_THRESHOLD			(60)
+#define DEF_FREQUENCY_DOWN_THRESHOLD		(26)
 #define FREQ_STEP_DOWN 						(160000)
-#define FREQ_SLEEP_MAX 						(320000)
-#define FREQ_AWAKE_MIN 						(480000)
+#define FREQ_SLEEP_MAX 						(422400)
+#define FREQ_AWAKE_MIN 						(576000)
 #define FREQ_STEP_UP_SLEEP_PERCENT 			(20)
 
 /*
